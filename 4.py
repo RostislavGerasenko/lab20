6ba6fc8aa62d79fdb4c01fe5dfc0be1e8d393540
RostislavGerasenko <91475179+RostislavGerasenko@users.noreply.github.com>
2021-12-20 13:02:53 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -3,9 +3,9 @@ farthest = (0,0)
 for i in range(n):
     x = int(input(f"Enter the x coordinate of point number {i+1}: "))
     y = int(input(f"Enter the y coordinate of point number {i+1}: "))
-    if not x > 0 or y < 0:
+    if x > 0 or y < 0:
         continue
     distance = farthest[0]**2 + farthest[1]**2
     if x**2 + y**2 > distance:
         farthest = (x,y)
-print(f"The point in the second quadrant that is the farthest from origin is ({x}, {y})")
+print(f"The point in the second quadrant that is the farthest from origin is ({farthest[0]}, {farthest[1]})")
